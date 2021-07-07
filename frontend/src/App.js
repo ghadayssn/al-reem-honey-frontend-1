976885386809592b9ef1efd00d1e0d7f6086dfea
RihamBadra <rihamHbadra@gmail.com>
2021-07-07 14:03:45 +0300

    Contact Us added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/footer";
+import ContactForm from "./pages/ContactForm";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Footer />
+      <ContactForm />
     </div>
   );
 }
