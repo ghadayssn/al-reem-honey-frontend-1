4e98132db56b271fa46beed1e69d976bb4fd0d88
RihamBadra <rihamHbadra@gmail.com>
2021-07-07 15:57:28 +0300

    updated css

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -7,7 +7,9 @@ export default function About() {
     <div class="flexbox">
       <div class="one">
         <h3>عسل الريم</h3>
-        <image src={honey} alt="honey pic" />
+        <div class="pic">
+          <img src={honey} alt="honey pic" />
+        </div>
       </div>
       <div class="two">lorem</div>
     </div>
