049fad12129639773a6b0dfaceedba4e5c0cd383
RihamBadra <rihamHbadra@gmail.com>
2021-07-07 18:46:47 +0300

    footer edied

--- frontend/src/components/Footer/footer.js
+++ frontend/src/components/Footer/footer.js
@@ -1,45 +1,35 @@
 import "./footer.css";
 import React from "react";
-import LOGO from "./logo.jpg"
+import LOGO from "./logo.jpg";
 
 const Footer = () => (
-  <div className="footer">
+  <div class="box">
+    <div>
+      <img src={LOGO} width="20%" alt="LOGO" />
+      <p class="footer-company-name"> عسل الريم © 2021</p>
+    </div>
 
-    <footer class="footer-distributed">
+    <div>
+      <i class="fa fa-map-marker"></i>
+      <p>
+        <span>Aley,</span> Lebanon
+      </p>
+      <i class="fa fa-phone"></i>
+      <p>(+961)03692451</p>
+      <i class="fa fa-facebook"></i>
+      <p>
+        <a href="#">عسل الريم غروب</a>
+      </p>
+    </div>
 
-	<div class="footer-left">
-        <img src={LOGO} width="20%" alt="LOGO"/>
-        <p class="footer-company-name"> عسل الريم © 2021</p>
-	</div>
-
-	<div class="footer-center">
-		<div>
-			<i class="fa fa-map-marker"></i>
-			<p><span>Aley,</span> Lebanon</p>
-		</div>
-		<div>
-			<i class="fa fa-phone"></i>
-			<p>(+961)03692451</p>
-		</div>
-		<div>
-			<i class="fa fa-facebook"></i>
-			<p><a href="#">عسل الريم غروب</a></p>
-		</div>
-	</div>
-
-	<div class="footer-right">
-		<p class="footer-company-about">
-		<span>About the company</span>
-		Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
-		</p>
-		<div class="footer-icons">
-            <a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
-		</div>
-
-	</div>
-
-    </footer>
-</div>
+    <div>
+      <p class="footer-company-about">
+        <span>About the company</span>
+        Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod
+        convallis velit, eu auctor lacus vehicula sit amet.
+      </p>
+    </div>
+  </div>
 );
 
 export default Footer;
