d9e5ee935f078d1a4ce117d3427bf66386ee682b
Layal Haidar <layal.haidar1@hotmail.com>
2021-07-08 22:54:28 +0300

    styling best seller + update navbar overflow

--- frontend/src/components/BestSeller/bestSeller.js
+++ frontend/src/components/BestSeller/bestSeller.js
@@ -5,24 +5,24 @@ const BestSeller = (props) => {
   const position = props.pos;
   // console.log(position);
   return position ? (
-    <div className="main-wrapper">
+    <div className="main-wrapper card">
       <div className="product-wrapper">
         <h3 className="product-title">{props.name}</h3>
         <div className="product-description">{props.desc}</div>
       </div>
       <div className="product-image-container">
         <img
-          className="product-image"
+          className="product-image right"
           src="https://picsum.photos/800/301/?random"
           alt="2"
         />
       </div>
     </div>
   ) : (
-    <div className="main-wrapper">
+    <div className="main-wrapper ">
       <div className="product-image-container">
         <img
-          className="product-image"
+          className="product-image left"
           src="https://picsum.photos/800/301/?random"
           alt="2"
         />
