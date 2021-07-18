d61869ea2a70eed9fe29c142ed17e1f464d77da1
RihamBadra <rihamHbadra@gmail.com>
2021-07-18 15:40:31 +0300

    route changed

--- frontend/src/pages/ContactForm.js
+++ frontend/src/pages/ContactForm.js
@@ -13,13 +13,14 @@ const ContactForm = () => {
       email: email.value,
       message: message.value,
     };
-    let response = await fetch("http://localhost:5000/contact", {
+    let response = await fetch("http://localhost:5000/contact/sendMail", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
       },
       body: JSON.stringify(details),
     });
+    console.log(response);
     setStatus("Submit");
     let result = await response.json();
     alert(result.status);
