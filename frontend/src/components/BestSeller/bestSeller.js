7104046403f583f79be82e7af975837439b09272
layal <layal.haidar136@gmail.com>
2021-07-20 14:51:25 +0300

    add headings to the home page

--- frontend/src/components/BestSeller/bestSeller.js
+++ frontend/src/components/BestSeller/bestSeller.js
@@ -5,31 +5,36 @@ const BestSeller = (props) => {
   const position = props.pos;
 
   return position ? (
-    <div className="main-wrapper card">
-      <div className="product-wrapper">
-        <h3 className="product-title">{props.name}</h3>
-        <div className="product-description">{props.desc}</div>
-      </div>
-      <div className="product-image-container">
-        <img
-          className="product-image right"
-          src={`http://localhost:5000/images/${props.urlImage}`}
-          alt="alt image"
-        />
+    <div className="MAIN">
+      <h1 className="section-title"> المنتجات الاكثر مبيعا </h1>
+      <div className="main-wrapper card">
+        <div className="product-wrapper">
+          <h3 className="product-title">{props.name}</h3>
+          <div className="product-description">{props.desc}</div>
+        </div>
+        <div className="product-image-container">
+          <img
+            className="product-image right"
+            src={`http://localhost:5000/images/${props.urlImage}`}
+            alt="alt image"
+          />
+        </div>
       </div>
     </div>
   ) : (
-    <div className="main-wrapper ">
-      <div className="product-image-container">
-        <img
-          className="product-image left"
-          src={`http://localhost:5000/images/${props.urlImage}`}
-          alt="2"
-        />
-      </div>
-      <div className="product-wrapper">
-        <h3 className="product-title">{props.name}</h3>
-        <div className="product-description">{props.desc}</div>
+    <div className="MAIN">
+      <div className="main-wrapper ">
+        <div className="product-image-container">
+          <img
+            className="product-image left"
+            src={`http://localhost:5000/images/${props.urlImage}`}
+            alt="2"
+          />
+        </div>
+        <div className="product-wrapper">
+          <h3 className="product-title">{props.name}</h3>
+          <div className="product-description">{props.desc}</div>
+        </div>
       </div>
     </div>
   );
--- frontend/src/components/LatestArticles/latestArticles.js
+++ frontend/src/components/LatestArticles/latestArticles.js
@@ -4,9 +4,13 @@ import Grid from "@material-ui/core/Grid";
 export default function LatestArticles() {
   return (
     <div>
-      <Grid container style={{marginTop: '2rem', marginBottom: '2rem' }}>
-        <Grid item xs={4} style={{padding:"10px" }}>
-          <Grid container style={{boxShadow: "0px 0px 8px 0px",borderRadius:"4px"}}>
+      <h1 className="section-title"> أحدث المقالات</h1>
+      <Grid container style={{ marginTop: "2rem", marginBottom: "2rem" }}>
+        <Grid item xs={4} style={{ padding: "10px" }}>
+          <Grid
+            container
+            style={{ boxShadow: "0px 0px 8px 0px", borderRadius: "4px" }}
+          >
             <Grid item xs={12}>
               <img
                 className="slider-img"
@@ -14,15 +18,20 @@ export default function LatestArticles() {
                 alt="1"
               />
             </Grid>
-            <Grid xs={12} style={{padding:"0px 40px"}}>
-              <div>مجدرة وعسل لها فوائد عديدة لكنني لا أعرفها.
-   كان جدي يحبها كثيرا. وأنا منذ صغري اعتدت عليها لأجل جدي.
-   احب جدي كثيرا فعمره يناهز ال٩٠ ولا زال يتمتع بحس فكاهي مميز وصحته جيدة جدا.</div>
+            <Grid xs={12} style={{ padding: "0px 40px" }}>
+              <div>
+                مجدرة وعسل لها فوائد عديدة لكنني لا أعرفها. كان جدي يحبها كثيرا.
+                وأنا منذ صغري اعتدت عليها لأجل جدي. احب جدي كثيرا فعمره يناهز
+                ال٩٠ ولا زال يتمتع بحس فكاهي مميز وصحته جيدة جدا.
+              </div>
             </Grid>
           </Grid>
         </Grid>
-        <Grid item xs={4} style={{padding:"10px"}}>
-          <Grid container style={{boxShadow: "0px 0px 8px 0px",borderRadius:"4px"}}>
+        <Grid item xs={4} style={{ padding: "10px" }}>
+          <Grid
+            container
+            style={{ boxShadow: "0px 0px 8px 0px", borderRadius: "4px" }}
+          >
             <Grid item xs={12}>
               <img
                 className="slider-img"
@@ -30,15 +39,20 @@ export default function LatestArticles() {
                 alt="1"
               />
             </Grid>
-            <Grid xs={12} style={{padding:"0px 40px"}}>
-              <div>مجدرة وعسل لها فوائد عديدة لكنني لا أعرفها.
-   كان جدي يحبها كثيرا. وأنا منذ صغري اعتدت عليها لأجل جدي.
-   احب جدي كثيرا فعمره يناهز ال٩٠ ولا زال يتمتع بحس فكاهي مميز وصحته جيدة جدا.</div>
+            <Grid xs={12} style={{ padding: "0px 40px" }}>
+              <div>
+                مجدرة وعسل لها فوائد عديدة لكنني لا أعرفها. كان جدي يحبها كثيرا.
+                وأنا منذ صغري اعتدت عليها لأجل جدي. احب جدي كثيرا فعمره يناهز
+                ال٩٠ ولا زال يتمتع بحس فكاهي مميز وصحته جيدة جدا.
+              </div>
             </Grid>
           </Grid>
         </Grid>
-        <Grid item xs={4} style={{padding:"10px"}}>
-          <Grid container style={{boxShadow: "0px 0px 8px 0px",borderRadius:"4px"}}>
+        <Grid item xs={4} style={{ padding: "10px" }}>
+          <Grid
+            container
+            style={{ boxShadow: "0px 0px 8px 0px", borderRadius: "4px" }}
+          >
             <Grid item xs={12}>
               <img
                 className="slider-img"
@@ -46,10 +60,12 @@ export default function LatestArticles() {
                 alt="1"
               />
             </Grid>
-            <Grid xs={12} style={{padding:"0px 40px"}}>
-              <div>مجدرة وعسل لها فوائد عديدة لكنني لا أعرفها.
-   كان جدي يحبها كثيرا. وأنا منذ صغري اعتدت عليها لأجل جدي.
-   احب جدي كثيرا فعمره يناهز ال٩٠ ولا زال يتمتع بحس فكاهي مميز وصحته جيدة جدا.</div>
+            <Grid xs={12} style={{ padding: "0px 40px" }}>
+              <div>
+                مجدرة وعسل لها فوائد عديدة لكنني لا أعرفها. كان جدي يحبها كثيرا.
+                وأنا منذ صغري اعتدت عليها لأجل جدي. احب جدي كثيرا فعمره يناهز
+                ال٩٠ ولا زال يتمتع بحس فكاهي مميز وصحته جيدة جدا.
+              </div>
             </Grid>
           </Grid>
         </Grid>
